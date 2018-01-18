79fc6ce3379bbaad139fd7449d3cd589cee5d837
Will <github@will.systems>
2018-01-17 21:42:24 -0600

    input/lib/Common: error message differences

--- tinymu-input-events/lib/CommonFunctions.js
+++ tinymu-input-events/lib/CommonFunctions.js
@@ -4,28 +4,28 @@
  * General functions used on the ranvier-input-events bundle
  */
 
-const srcPath = '../../../src/'
+const srcPath = '../../../src/';
 const Config  = require(srcPath + 'Config');
 
 /**
  * @param {string} name
  * @return {boolean}
  */
-exports.validateName = function(name) {
+exports.validateName = (name) => {
   const maxLength = Config.get('maxAccountNameLength');
   const minLength = Config.get('minAccountNameLength');
 
   if (!name) {
-    return 'Please enter a name.';
+    return 'Your name cannot be blank.';
   }
   if (name.length > maxLength) {
-    return 'Too long, try a shorter name.';
+    return `Your name needs to be less than ${maxLength} characters.`;
   }
   if (name.length < minLength) {
-    return 'Too short, try a longer name.';
+    return `Your name needs to be greater than ${minLength} characters.`;
   }
   if (!/^[a-z]+$/i.test(name)) {
-    return 'Your name may only contain A-Z without spaces or special characters.';
+    return 'Your name may contain only A-Z without spaces or special characters.';
   }
   return false;
-}
+};
